7c8f1df8b20cae5e912424735a19543ea5f0ac30
Quentin Vanhauteghem <quentvht@hotmail.com>
2025-05-01 22:31:25 +0200

    fix: solve issue with score when 0

--- src/components/companies/CompanyScore/index.tsx
+++ src/components/companies/CompanyScore/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { useUnit } from 'effector-react'
 import { companyScoresStores } from '../../../models/company/scores'
-import { Rating } from "primereact/rating";
+import { Rating } from 'primereact/rating'
 
 export const CompanyScore: React.FC = () => {
-    const score = useUnit(companyScoresStores.$scores)?.score ?? undefined;
+    const score = useUnit(companyScoresStores.$scores)?.score ?? undefined
     return (
-        <div className={`flex align-items-center justify-content-center border-1 p-2 ${score === undefined ? 'border-warning' : 'border-solid'}`}>
-            <Rating value={score ? score + 2 : 0} disabled cancel={false} /> 
+        <div
+            className={`flex align-items-center justify-content-center border-1 p-2 ${score === undefined ? 'border-warning' : 'border-solid'}`}
+        >
+            <Rating
+                value={score !== undefined ? score + 2 : 0}
+                disabled
+                cancel={false}
+            />
         </div>
-    );
+    )
 }
