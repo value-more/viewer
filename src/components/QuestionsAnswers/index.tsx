27318c58c1361e8f5ef36c2eea152a07e894281f
Quentin Vanhauteghem <quentvht@hotmail.com>
2025-05-03 18:05:32 +0200

    feat: Translate questions/answers

--- src/components/QuestionsAnswers/index.tsx
+++ src/components/QuestionsAnswers/index.tsx
@@ -1,81 +1,114 @@
 import React, { useEffect, useState } from 'react'
-import { Question } from './types';
-import { api } from '../../api/invData';
-import { InputTextarea } from 'primereact/inputtextarea';
-import { InfoIcon } from '../InfoIcon';
+import { Answers, TranslatedValue, Question } from './types'
+import { api } from '../../api/invData'
+import { InputTextarea } from 'primereact/inputtextarea'
+import { InfoIcon } from '../InfoIcon'
+import { useTranslation } from 'react-i18next'
 
 interface QuestionsAnswersProps {
     /**
      * API for questions config and answers per cik usually
      */
-    apiUrls: { questions: string; answers: string };
-    readonly?: boolean;
+    apiUrls: { questions: string; answers: string }
+    readonly?: boolean
 }
 
-const timeouts: {[key: string]: any} = {};
+const timeouts: { [key: string]: any } = {}
 
-export const QuestionsAnswers: React.FC<QuestionsAnswersProps> = ({ apiUrls, readonly }) => {
-    const [questions, setQuestions] = useState<Question[]>([]);
-    const [answers, setAnswers] = useState<{[key: string]: { answer: string; timestamp?: number; }}>({});
+export const QuestionsAnswers: React.FC<QuestionsAnswersProps> = ({
+    apiUrls,
+    readonly,
+}) => {
+    const {
+        i18n: { language },
+    } = useTranslation()
+    const [questions, setQuestions] = useState<Question[]>([])
+    const [answers, setAnswers] = useState<Answers>({})
 
     useEffect(() => {
         const getData = async () => {
-            const res = (await api(`${apiUrls.questions}?limit=1`))?.[0]?.rules?.questions || [];
-            setQuestions(res);
+            const res = (await api(`${apiUrls.questions}?limit=1`))?.[0]
+            const q = (res?.rules?.questions || []).map(
+                (v: any, i: number) => ({
+                    ...v,
+                    value: { de: v.value, en: res?.translations?.[i]?.en },
+                })
+            )
+            setQuestions(q)
 
-            const data = (await api(apiUrls.answers))
-                    .reduce((prev: {[key: string]: { answer: string; timestamp?: number; }}, c: { questionKey: string, answer: string, timestamp?: number; }) => {
-                        prev[c.questionKey] = { answer: c.answer, timestamp: c.timestamp };
-                        return prev;
-                    }, {});
-            setAnswers(data);
+            const data = (await api(`${apiUrls.answers}`)).reduce(
+                (
+                    prev: Answers,
+                    c: {
+                        questionKey: string
+                        answer: TranslatedValue
+                        timestamp?: number
+                    }
+                ) => {
+                    prev[c.questionKey] = {
+                        answer: c.answer,
+                        timestamp: c.timestamp,
+                    }
+                    return prev
+                },
+                {}
+            )
+            setAnswers(data)
         }
-        getData();
-    }, []);
-    
-    const save = (questionKey: string, answer: string) => {
-        setAnswers({ ...answers, [questionKey]: { answer, timestamp: Date.now() } });
+        getData()
+    }, [])
 
-        clearTimeout(timeouts[questionKey]);
+    const save = (questionKey: string, answer: string) => {
+        setAnswers({
+            ...answers,
+            [questionKey]: {
+                answer: { de: answer, en: answer },
+                timestamp: Date.now(),
+            },
+        })
+
+        clearTimeout(timeouts[questionKey])
         timeouts[questionKey] = setTimeout(() => {
             api(apiUrls.answers, {
                 method: 'POST',
                 body: JSON.stringify({
-                    questionKey, answer
-                })
-            });
-        }, 750 );
+                    questionKey,
+                    answer,
+                }),
+            })
+        }, 750)
     }
 
     return (
         <div>
-            {
-                questions.map( ({ key, value }) => {
-                    const timestamp = answers[key]?.timestamp;
-                    return (
-                    <div key={key} className='w-full'>
+            {questions.map(({ key, value }) => {
+                const timestamp = answers[key]?.timestamp
+                return (
+                    <div key={key} className="w-full">
                         <h4 className="flex">
-                            <div>{value}</div>
-                            {!readonly && (<div className='ml-auto mr-2 '>
-                                <InfoIcon editTimestamp={timestamp} />
-                            </div>)}
+                            <div>{value?.[language]}</div>
+                            {!readonly && (
+                                <div className="ml-auto mr-2 ">
+                                    <InfoIcon editTimestamp={timestamp} />
+                                </div>
+                            )}
                         </h4>
-                        {
-                            readonly ? 
-                            (<div>{answers[key]?.answer || ''}</div>) : 
-                            (
-                            <InputTextarea 
-                                autoResize 
-                                name={key} 
-                                className='w-full' 
-                                onChange={event => save(key, event.target.value)} 
-                                value={answers[key]?.answer || ''}
+                        {readonly ? (
+                            <div>{answers[key]?.answer?.[language] || ''}</div>
+                        ) : (
+                            <InputTextarea
+                                autoResize
+                                name={key}
+                                className="w-full"
+                                onChange={(event) =>
+                                    save(key, event.target.value)
+                                }
+                                value={answers[key]?.answer?.[language] || ''}
                             />
-                            )
-                        }
+                        )}
                     </div>
-                ) } )
-            }
+                )
+            })}
         </div>
-    );
+    )
 }
